e81320ef27e1db6bea7ccc9f99c02963ab2cc6f4
YeEeck <yechenkai@foxmail.com>
2021-11-14 20:57:56 +0800

    finish login&reg

--- src/App.vue
+++ src/App.vue
@@ -7,9 +7,7 @@
         <v-toolbar-title>星芒</v-toolbar-title>
       </v-app-bar>
       <v-main>
-
         <router-view />
-
       </v-main>
       <v-navigation-drawer v-model="open" absolute temporary>
         <v-list-item>
@@ -21,7 +19,12 @@
         <v-divider></v-divider>
 
         <v-list dense>
-          <v-list-item link v-for="item in items" :key="item.title">
+          <v-list-item
+            link
+            v-for="item in items"
+            :key="item.title"
+            @click="listClick(item.id)"
+          >
             <v-list-item-icon>
               <v-icon>{{ item.icon }}</v-icon>
             </v-list-item-icon>
@@ -40,10 +43,21 @@ export default {
   data: () => ({
     open: false,
     items: [
-      { icon: "", title: "刻印工房" },
-      { icon: "", title: "星台" },
+      { id: 1, icon: "", title: "刻印工房" },
+      { id: 2, icon: "", title: "星台" },
+      { id: 3, icon: "", title: "退出登录" },
     ],
   }),
+  methods: {
+    listClick(id) {
+      if (id == 3) {
+        localStorage.removeItem("account");
+        localStorage.removeItem("password");
+        sessionStorage.removeItem("logined");
+        this.$router.push("/login");
+      }
+    },
+  },
 };
 </script>
 
@@ -53,6 +67,6 @@ html {
 }
 #app {
   width: 100%;
-  background-color: #FCF8EC;
+  background-color: #fcf8ec;
 }
 </style>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -2,49 +2,123 @@
   <div>
     <v-row justify="center">
       <v-col cols="11" lg="4">
-        <v-card
-          class="text-center top-margin-medium padding-medium"
-          elevation="3"
-        >
-          <v-row>
-            <v-col>
-              <v-text-field
-                outlined
-                label="账户"
-                v-model="account"
-              ></v-text-field>
-              <v-text-field
-                outlined
-                label="密码"
-                v-model="password"
-              ></v-text-field>
-              <v-btn
-                color="#79A3B1"
-                dark
-                class="big-btn btn-white-text"
-                @click="login"
-                >登录</v-btn
-              >
-            </v-col>
-          </v-row>
-        </v-card>
+        <v-scroll-x-transition>
+          <v-card
+            v-show="showLoginPenal"
+            class="text-center top-margin-medium padding-medium"
+            elevation="3"
+            transition="fade-transition"
+          >
+            <v-row>
+              <v-col>
+                <v-text-field
+                  color="#79A3B1"
+                  outlined
+                  label="账户"
+                  v-model="account"
+                ></v-text-field>
+                <v-text-field
+                  type="password"
+                  color="#79A3B1"
+                  outlined
+                  label="密码"
+                  v-model="password"
+                ></v-text-field>
+              </v-col>
+            </v-row>
+            <v-row>
+              <v-col cols="4">
+                <v-btn
+                  color="#E4E3E3"
+                  class="fill-all-width btn-white-text"
+                  @click="changeShowPenal"
+                  >注册</v-btn
+                >
+              </v-col>
+              <v-col cols="8">
+                <v-btn
+                  color="#204051"
+                  dark
+                  class="fill-all-width btn-white-text"
+                  @click="login"
+                  >登录</v-btn
+                >
+              </v-col>
+            </v-row>
+          </v-card>
+        </v-scroll-x-transition>
+
+        <v-scroll-x-reverse-transition>
+          <v-card
+            v-show="showRegPenal"
+            class="text-center top-margin-medium padding-medium"
+            elevation="3"
+          >
+            <v-row>
+              <v-col>
+                <v-text-field
+                  color="#79A3B1"
+                  outlined
+                  label="账户"
+                  v-model="account"
+                ></v-text-field>
+                <v-text-field
+                  type="password"
+                  color="#79A3B1"
+                  outlined
+                  label="密码"
+                  v-model="password"
+                ></v-text-field>
+                <v-text-field
+                  type="password"
+                  color="#79A3B1"
+                  outlined
+                  label="再次输入密码"
+                  v-model="passwordCheck"
+                ></v-text-field>
+              </v-col>
+            </v-row>
+            <v-row>
+              <v-col cols="4">
+                <v-btn
+                  color="#E4E3E3"
+                  class="fill-all-width btn-white-text"
+                  @click="changeShowPenal"
+                  >登录</v-btn
+                >
+              </v-col>
+              <v-col cols="8">
+                <v-btn
+                  color="#204051"
+                  dark
+                  class="fill-all-width btn-white-text"
+                  @click="reg"
+                  >注册</v-btn
+                >
+              </v-col>
+            </v-row>
+          </v-card>
+        </v-scroll-x-reverse-transition>
       </v-col>
     </v-row>
 
-    <v-snackbar v-model="failSnackBar">
-      {{ failText }}
+    <v-snackbar v-model="snackBar1" top>
+      {{ snackText1 }}
     </v-snackbar>
   </div>
 </template>
 
 <script>
-import { login } from "../network/user";
+import { login, reg } from "../network/user";
 export default {
   data: () => ({
     account: "",
     password: "",
-    failSnackBar: false,
-    failText: "",
+    passwordCheck: "",
+    snackBar1: false,
+    snackText1: "",
+    showLoginPenal: true,
+    showRegPenal: false,
   }),
   mounted() {
     let account = localStorage.getItem("account");
@@ -59,8 +133,7 @@ export default {
     login() {
       login({ account: this.account, password: this.password }).then((res) => {
         if (res.data.success == "false") {
-          this.failText = "登录失败，账号或密码错误";
-          this.failSnackBar = true;
+          this.showSnackBar1("登录失败，账号或密码错误");
         } else {
           localStorage.setItem("account", this.account);
           localStorage.setItem("password", this.password);
@@ -68,6 +141,46 @@ export default {
           this.$router.push("/");
         }
       });
+    },
+    reg() {
+      if (this.account == "") {
+        this.showSnackBar1("用户名不能为空");
+        return;
+      }
+      if (this.password == "") {
+        this.showSnackBar1("密码不能为空");
+        return;
+      }
+      if (this.password == this.passwordCheck) {
+        reg({ account: this.account, password: this.password }).then((res) => {
+          if (res.data.success == "false") {
+            this.showSnackBar1("注册失败，该账号已存在");
+          } else {
+            this.showSnackBar1("注册成功");
+            this.changeShowPenal();
+          }
+        });
+      } else {
+        this.showSnackBar1("两次输入的密码不一致");
+      }
+    },
+    changeShowPenal() {
+      let then = this;
+      if (this.showLoginPenal == true) {
+        this.showLoginPenal = false;
+        setTimeout(function () {
+          then.showRegPenal = true;
+        }, 500);
+      } else {
+        this.showRegPenal = false;
+        setTimeout(function () {
+          then.showLoginPenal = true;
+        }, 500);
+      }
+    },
+    showSnackBar1(text) {
+      this.snackText1 = text;
+      this.snackBar1 = true;
     },
   },
 };
@@ -87,5 +200,8 @@ export default {
 .big-btn {
   width: 15rem;
   height: 2rem;
+}
+.fill-all-width {
+  width: 100%;
 }
 </style>
